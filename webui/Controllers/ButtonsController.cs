b61849e55fb1b661c145d3b45ed840c623362147
Peter Shaw <shawty@shawty.demon.co.uk>
2013-10-09 23:01:45 +0100

    Added in first part of buttons views and demos, still to do: Finish off button grouping and add section on drop down and split buttons. Then add all sections for scaffolding, Components and TBMVC - P.J.S

--- webui/Controllers/ButtonsController.cs
+++ webui/Controllers/ButtonsController.cs
@@ -9,5 +9,30 @@ namespace webui.Controllers
       return View();
     }
 
+    public ActionResult Styles()
+    {
+      return View();
+    }
+
+    public ActionResult Icons()
+    {
+      return View();
+    }
+
+    public ActionResult Sizing()
+    {
+      return View();
+    }
+
+    public ActionResult Groups()
+    {
+      return View();
+    }
+
+    public ActionResult Dropdowns()
+    {
+      return View();
+    }
+
   }
 }
